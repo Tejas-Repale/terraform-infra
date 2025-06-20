0e13df1b6e3f4289c15817c20916c67df387020c
Tejas-Repale <tejasrepale285@gmail.com>
2025-06-20 10:15:45 +0000

    fix: formatting files with terraform fmt

--- vpc.tf
+++ vpc.tf
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 # VPC
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
@@ -10,10 +10,10 @@ resource "aws_vpc" "main" {
 
 # Public Subnets
 resource "aws_subnet" "public" {
-  count             = 2
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = element(["10.0.1.0/24", "10.0.2.0/24"], count.index)
-  availability_zone = element(["ap-south-1a", "ap-south-1b"], count.index)
+  count                   = 2
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = element(["10.0.1.0/24", "10.0.2.0/24"], count.index)
+  availability_zone       = element(["ap-south-1a", "ap-south-1b"], count.index)
   map_public_ip_on_launch = true
 
   tags = {
@@ -39,10 +39,10 @@ resource "aws_subnet" "private_subnet_2" {
   cidr_block              = "10.0.4.0/24"
   availability_zone       = "ap-south-1b"
   map_public_ip_on_launch = false
-
-  tags = {
-    Name = "private-subnet-2"
-=======
+}
+tags = {
+  Name = "private-subnet-2"
+}
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
@@ -51,6 +51,5 @@ resource "aws_subnet" "public_subnet" {
 
   tags = {
     Name = "PublicSubnet"
->>>>>>> 99ecdeff4d07acd3ddf2a8aff16cf8a7680eee25
   }
 }
